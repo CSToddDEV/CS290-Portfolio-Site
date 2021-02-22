ed9fee7b6f31dc16909de94d70d614c59645b27a
Calvin <CSTodd88@gmail.com>
2021-02-22 10:50:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var current_slide;
 var scroll_buttons = document.getElementsByClassName("about-button");
 var slide = 0;
-console.log(window.location.pathname, "Patname")
 if (window.location.pathname === "/~toddcal/" || window.location.pathname === "/~toddcal/index.html"){
   slideShow()
 };
